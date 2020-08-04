86967a9b3e6170abc142b80a7d9033cab1595ef6
kevinS-code <69072479+kevinS-code@users.noreply.github.com>
2020-08-04 21:39:22 +0700

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,36 @@
+#temperature.py
+from tkinter import *
+from tkinter import ttk
+
+GUI = Tk()
+GUI.geometry('500x500')
+GUI.title('แปลงอุณหภูมิ')
+
+def All():
+	c = int(P.get())
+	KAns = c + K
+	FAns = c + F
+	AllAnswer.get(f'คำตอบคือ: {KAns:,.2f}')
+
+#############################
+
+FONT = ('Angsana New',20)
+P = StringVar()
+K = int(273.15)
+F = int(32)
+AllAnswer = StringVar()
+AllAnswer.set('----------')
+
+CL1 = ttk.Label(GUI,text='โปรแกรมแปลงค่าอุณหภูมิ',font=FONT)
+CL1.pack()
+
+CE1 = ttk.Entry(GUI,textvariable=P,font=FONT)
+CE1.pack()
+
+CB1 = ttk.Button(GUI,text='คำนวณ',command=All)
+CB1.pack()
+
+CL2 = ttk.Label(GUI,textvariable=AllAnswer,font=FONT)
+CL2.pack()
+
+GUI.mainloop()
