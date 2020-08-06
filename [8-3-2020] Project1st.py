89c1cecc2be97fcae1e0114a65fbd07b0885f75e
kevinS-code <69072479+kevinS-code@users.noreply.github.com>
2020-08-06 19:55:50 +0700

    Update [8-3-2020] Project1st.py

--- [8-3-2020] Project1st.py
+++ [8-3-2020] Project1st.py
@@ -1,18 +1,18 @@
 #Project1st.py
-from tkinter import *
-from tkinter import ttk,messagebox
+from tkinter import * 
+from tkinter import ttk,messagebox #(Date:8/3/2020)
 
 def Calcurate():
 	input1 = int(value1.get())
 	input2 = int(value2.get())
 	res = input1 + input2
-	result.set(f'คำตอบคือ {res:,d}')
+	result.set(f'คำตอบคือ {res:,d}')#(Date:8/3/2020)
 
 '''def CalcuratE():
 	input1 = int(value1.get())
 	input2 = int(value2.get())
 	ser = input1 + input2
-	Result.set(f'คำตอบคือ {ser:,d}')'''
+	Result.set(f'คำตอบคือ {ser:,d}')'''#(Date:8/3/2020)
 
 def Plus():
 	messagebox.showinfo('วิธีบวก','นี่คือตัวอย่าง 1 + 1 = 2')
@@ -21,25 +21,36 @@ def Minus():
 def Multiple():
 	messagebox.showinfo('วิธีคูณ','นี่คือตัวอย่าง 29 * 35= 1015')
 def Divie():
-	messagebox.showinfo('วิธีบวก','นี่คือตัวอย่าง 18 / 6 = 3')
-######################Cricle#######################
+	messagebox.showinfo('วิธีบวก','นี่คือตัวอย่าง 18 / 6 = 3')#(Date:8/3/2020)
+######################Cricle#######################(Date:8/3/2020)
 def Cricle(): #ถ้าฟังก์ชั้นใช้แล้วจบ ตัวแปรซ้ำได้ไม่มีผล ถ้าฟังก์ชั้นยังไม่จบ จะมีผล
 	x = int(KK.get())
 	sec = Pi * (x ** 2) #สามารถทำแบบนี้ได้เลย sec = 3.14 * (x ** 2) ไม่จำเป็นต้องไปแทนค่าเป็นค่า Pi หรือ ตัดวงเล็บไปก็ได้ผลลัพท์เดิม
 	#sec = Pi * x ** 2 หรือแบบนี้ก็ได้ sec = 3.14 * x ** 2 [** คือ ยกกำลัง]
 	result1.set(f'คำตอบคือ {sec:,.2f}') #:,.xf<--->x คือ จำนวนทศนิยม 
-######################Cone#######################
+######################Cone#######################(Date:8/3/2020)
 def Cone():
 	f = float(F.get())
 	h = float(H.get())
 	Ans = 1/3 * f *h
 	SrCone.set(f'คำตอบคือ {Ans:,.2f}')
-######################TA#######################
+######################TA#########################
 def TA():
 	f = float(F.get())
 	h = float(H.get())
 	Ans = 1/2 * f * h
 	Answer.set(f'คำตอบคือ: {Ans:,.2f}')
+#################################################
+def RbsF():
+	fill_inB = float(Base_length.get())
+	fill_inH = float(Height.get())
+	Cal = fill_inB * fill_inH
+	ResultRbs.set(f'Result is: {Cal:,.2f}')
+#################################################
+def RbsFV_2():
+	Fill_inPODL = float(Product_of_diagonal_length.get())
+	Cal1 = 1/2 * Fill_inPODL
+	ResultRbs1.set(f'Result is: {Cal1:,.2f}')
 #######################New File####################################
 def New1File():
 	GUI2 = Tk()
@@ -63,8 +74,8 @@ def New1File():
 	CResult.pack()'''
 
 	GUI2.mainloop()
-#######################New File####################################
-GUI = Tk() #GUI เป็กการตั้งชื่อ เพื่อที่จะใช้ Tk() 
+#######################New File#####################################(Date:8/3/2020)
+GUI = Tk() #GUI เป็กการตั้งชื่อ เพื่อที่จะใช้ Tk()  
 
 GUI.title('CalcurateProgram')
 GUI.geometry('500x500')
@@ -75,13 +86,17 @@ Tab.pack(fill=BOTH)
 
 F1 = Frame(Tab)
 F2 = Frame(Tab)
-F3 = Frame(Tab)
+F3 = Frame(Tab) #(Date:8/3/2020)
 F4 = Frame(Tab)
+F5 = Frame(Tab)
+F6 = Frame(Tab)
 
 Tab.add(F1,text='พื้นฐานคำนวณ')
 Tab.add(F2,text='วงกลม')
-Tab.add(F3,text='กรวย')
+Tab.add(F3,text='กรวย') #(Date:8/3/2020)
 Tab.add(F4,text='สามเหลี่ยม')
+Tab.add(F5,text='สี่เหลี่ยมขนมเปียกปูน')
+Tab.add(F6,text='สี่เหลี่ยมขนมเปียกปูนV_2')
 
 menubar = Menu(GUI)
 GUI.config(menu=menubar)
@@ -97,9 +112,7 @@ calc.add_command(label='การลบ',command=Minus)
 calc.add_command(label='การคูณ',command=Multiple)
 calc.add_command(label='การหาร',command=Divie)
 menubar.add_cascade(label='การคำนวณ',menu=calc)
-
-
-
+#########################################################(Date:8/3/2020)
 FONT = ('Angsana New',20)
 
 L1 = ttk.Label(F1,text='โปรแกรมคำนวณ',font=FONT)#Lable ตัวหนังสือ
@@ -158,7 +171,7 @@ SrConeB1 = ttk.Button(F3,text='คำนวณ',command=Cone)
 SrConeB1.pack(ipadx=10,ipady=5,padx=3,pady=3)
 
 SwrCone = ttk.Label(F3,textvariable=SrCone,font=FONT)
-SwrCone.pack()
+SwrCone.pack()#Date(8/3/2020)
 ######################F4#######################
 F = StringVar()
 H = StringVar()
@@ -178,6 +191,45 @@ TAB1 = ttk.Button(F4,text='คำนวณ',command=TA)
 TAB1.pack(ipadx=10,ipady=5,padx=3,pady=3)
 
 TAL2 = ttk.Label(F4,textvariable=Answer,font=FONT)
-TAL2.pack()
+TAL2.pack()#Date
+######################F5Variable###############
+Base_length = StringVar()
+Height = StringVar()
+Product_of_diagonal_length = StringVar()
+ResultRbs = StringVar()
+ResultRbs.set('---------------')
+ResultRbs1 = StringVar()
+ResultRbs1.set('---------------')
+###############################################
+######################F5#######################
+RbsLabel = ttk.Label(F5,text='โปรแกรมคำนวณ พท.สี่เหลี่ยมขนมเปียกปูน',font=FONT)
+RbsLabel.pack()
 
-GUI.mainloop()
+RbsEntry = ttk.Entry(F5,textvariable=Base_length,font=FONT)
+RbsEntry.pack()
+
+RbsEntry1 = ttk.Entry(F5,textvariable=Height,font=FONT)
+RbsEntry1.pack()
+
+RbsButton = ttk.Button(F5,text='Calculate',command=RbsF)
+RbsButton.pack()
+
+RbsLabel1 = ttk.Label(F5,textvariable=ResultRbs,font=FONT)
+RbsLabel1.pack()
+
+######################F6#######################
+RbsLabel2 = ttk.Label(F6,text='โปรแกรมคำนวณ พท.สี่เหลี่ยมขนมเปียกปูน',font=FONT)
+RbsLabel2.pack()
+
+RbsEntry2 = ttk.Entry(F6,textvariable=Product_of_diagonal_length,font=FONT)
+RbsEntry2.pack()
+
+RbsButton1 = ttk.Button(F6,text='Calculate',command=RbsFV_2)
+RbsButton1.pack()
+
+RbsLabel3 = ttk.Label(F6,textvariable=ResultRbs1,font=FONT)
+RbsLabel3.pack()
+
+###############################################
+
+GUI.mainloop() #Last update 8/6/2020-6:56PM
