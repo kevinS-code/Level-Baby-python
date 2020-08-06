e55838b30a6f9a3d8985cacf8590d4034b28aaca
kevinS-code <69072479+kevinS-code@users.noreply.github.com>
2020-08-06 19:33:12 +0700

    Add files via upload

---
+++ DateUpdateProject1st.py
@@ -0,0 +1,22 @@
+#DateUpdateProject1st.py
+######################Date#####################
+
+(Date:8/3/2020)
+main
+(Date:8/4/2020)
+Edit Name #CalcurateProgram.py to Project1st.py
+Add
+def TA
+F4 = Frame(TAB)
+Tab.add(F4)
+####F4####
+(Date:8/6/2020:6_56PM)
+Add  
+F5Variable
+F5 = Frame(TAB),F6 = Frame(TAB)
+Tab.add(F5),Tab.add(F6)
+def Rbs
+def RbsFV_2
+####F5####,####F6####
+
+###############################################
