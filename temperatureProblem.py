25b908e3066347002a53474aaf2946ffd91091cd
kevinS-code <69072479+kevinS-code@users.noreply.github.com>
2020-08-09 11:13:41 +0700

    Rename temperatureProblem.py to [8-9-2020]temperatureProblem.py

--- temperatureProblem.py
+++
@@ -1,7 +0,0 @@
-#temperatureProblem.py
-'''File "C:\Users\HP\Desktop\Python\Work\temperature.py", line 138
-SyntaxError: Non-UTF-8 code starting with '\xe0' in file C:\Users\HP\Desktop\Python\Work\temperature.py on line 138, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
-[Finished in 0.2s with exit code 1]
-[shell_cmd: python -u "C:\Users\HP\Desktop\Python\Work\temperature.py"]
-[dir: C:\Users\HP\Desktop\Python\Work]
-[path: C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\Program Files (x86)\AOMEI Backupper;C:\Program Files (x86)\IVI Foundation\VISA\WinNT\Bin;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\nodejs\;C:\Program Files (x86)\NVIDIA Corporation\PhysX\Common;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Python38\Scripts\;C:\Python38\;C:\Users\HP\AppData\Local\Programs\Python\Python38-32\Scripts\;C:\Users\HP\AppData\Local\Programs\Python\Python38-32\;C:\Users\HP\AppData\Local\Microsoft\WindowsApps;C:\Users\HP\AppData\Local\atom\bin;C:\AppServ\Apache24\bin;C:\AppServ\php5;C:\AppServ\MySQL\bin;C:\Users\HP\AppData\Local\Microsoft\WindowsApps;C:\Users\HP\AppData\Roaming\npm;]'''
